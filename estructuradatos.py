dda45fa776b3cf3b9cede5b3852bd65d4fad15fa
Valhia <ale.aquinocasas@gmail.com>
2025-05-23 21:26:02 -0500

    version4

--- estructuradatos.py
+++ estructuradatos.py
@@ -1,1 +1,10 @@
 print("estructura de datos")
+
+cola = ColaTareas()
+
+cola.agregar_tarea("Enviar correo")
+cola.agregar_tarea("Hacer backup")
+cola.ver_tareas()
+
+cola.procesar_tarea()
+cola.ver_tareas()
